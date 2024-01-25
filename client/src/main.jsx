2c6ba671459e2db6aa001f4293b55f2a39b0633b
FidanHuseynzada <tu7m7m7rr@code.edu.az>
2024-01-25 17:58:08 +0400

    exam

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import WishlistItemContextProvider from "./context/WishlistItemContext.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WishlistItemContextProvider>
-      <App />
-    </WishlistItemContextProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
